d5c0321a352ed08a89ff1e2d0c4f03fa07a9b175
Bailey Ellul <baileyellul@gmail.com>
2022-08-02 14:17:08 -0400

    Initial Commit

---
+++ Advocate's choice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Advocate's choice
+//
+//  Created by Bailey Ellul on 8/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
