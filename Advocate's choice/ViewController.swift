e303850177ab455c9686e5c67b79a1939c0e8637
Ankita Saha <sahaankita348@gmail.com>
2022-08-04 12:15:56 -0400

    thumbs up/down

--- Advocate's choice/ViewController.swift
+++ Advocate's choice/ViewController.swift
@@ -14,6 +14,41 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func up(_ sender: UIButton) {
+    }
+    
+    
 
+    @IBAction func down(_ sender: UIButton) {
+    }
+    
+
+    @IBAction func up1(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func down1(_ sender: UIButton) {
+    }
+    
+    
+    
+    @IBAction func up2(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func down2(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func up3(_ sender: UIButton) {
+    }
+    
+    
+    
+    @IBAction func down3(_ sender: UIButton) {
+    }
+    
+    
 }
 
